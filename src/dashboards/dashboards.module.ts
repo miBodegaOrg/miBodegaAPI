f2dd7fcc2ae6bb37e19182d3f1186856473fe02b
Gonzalo Sakuda <gonchissn@gmail.com>
2024-06-12 14:09:13 -0500

    feat: add sales dashboard endpoint

--- src/dashboards/dashboards.module.ts
+++ src/dashboards/dashboards.module.ts
@@ -3,11 +3,13 @@ import { DashboardsController } from './dashboards.controller';
 import { DashboardsService } from './dashboards.service';
 import { Product, ProductSchema } from 'src/schemas/Product.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Sale, SaleSchema } from 'src/schemas/Sales.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Product.name, schema: ProductSchema }
+      { name: Product.name, schema: ProductSchema },
+      { name: Sale.name, schema: SaleSchema}
     ])
   ],
   controllers: [DashboardsController],
--- src/dashboards/dashboards.service.ts
+++ src/dashboards/dashboards.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Product } from 'src/schemas/Product.schema';
+import { Sale } from 'src/schemas/Sales.schema';
 
 @Injectable()
 export class DashboardsService {
 
     constructor(
         @InjectModel(Product.name) private productModel: Model<Product>,
+        @InjectModel(Sale.name) private saleModel: Model<Sale>,
     ) {}
 
     getSalesByCategory() {
@@ -52,6 +54,44 @@ export class DashboardsService {
                   "$divide": ["$categories.totalSales", "$totalSalesAllCategories"]
                 }
               }
+            }
+          ]);
+    }
+
+    getSalesDashboard(period: "day" | "week" | "month" | "year") {
+
+        if (period !== "day" && period !== "week" && period !== "month" && period !== "year") throw new HttpException('Invalid period', 400);
+
+        const dateFilter = {
+            "day": { filter: new Date(new Date().getTime() - 24*60*60*1000), format: "%H:00" },
+            "week": { filter: new Date(new Date().setDate(new Date().getDate() - 7)), format: "%m-%d" },
+            "month": { filter: new Date(new Date().setDate(new Date().getDate() - 30)), format: "%m-%d" },
+            "year": { filter: new Date(new Date().setFullYear(new Date().getFullYear() - 1)), format: "%Y-%m" }
+        };
+
+        return this.saleModel.aggregate([
+            {
+              "$match": {
+                "createdAt": {
+                  "$gte": dateFilter[period].filter
+                },
+                "status": "paid"
+              }
+            },
+            {
+              "$group": {
+                "_id": {
+                  "$dateToString": {
+                    "format": dateFilter[period].format,
+                    "date": "$createdAt",
+                    "timezone": "America/Lima"
+                  }
+                },
+                "sales": { "$sum": 1 }
+              }
+            },
+            {
+              "$sort": { "_id": 1 }
             }
           ]);
     }
