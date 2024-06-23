17cf83c26e5b213d2b4baa7b219115bccd4d31f4
Gonzalo Sakuda <gonchissn@gmail.com>
2024-06-23 18:30:17 -0500

    fix: auth module in dashboards

--- src/dashboards/dashboards.module.ts
+++ src/dashboards/dashboards.module.ts
@@ -4,9 +4,11 @@ import { DashboardsService } from './dashboards.service';
 import { Product, ProductSchema } from 'src/schemas/Product.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Sale, SaleSchema } from 'src/schemas/Sales.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([
       { name: Product.name, schema: ProductSchema },
       { name: Sale.name, schema: SaleSchema}
