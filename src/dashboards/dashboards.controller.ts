8b3556de3cc1e276ecc74abe3bbacfe2e9ae2701
Gonzalo Sakuda <gonchissn@gmail.com>
2024-09-05 15:50:38 -0500

    add inventory dashboard

--- src/dashboards/dashboards.controller.ts
+++ src/dashboards/dashboards.controller.ts
@@ -2,31 +2,44 @@ import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { DashboardsService } from './dashboards.service';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { PermissionsGuard, Permissions } from 'src/auth/guards/permission.guard';
+import {
+  PermissionsGuard,
+  Permissions,
+} from 'src/auth/guards/permission.guard';
 
 @Controller('api/v1/dashboards')
 @ApiTags('Dashboards')
 export class DashboardsController {
-    constructor(private dashboardsService: DashboardsService) {}
+  constructor(private dashboardsService: DashboardsService) {}
 
-    @Get('categories')
-    @UseGuards(AuthGuard(), PermissionsGuard)
-    @Permissions('dashboards.leer')
-    getDashboardCategories(@Req() req) {
-       return this.dashboardsService.getSalesByCategory(req.user);
-    }
+  @Get('categories')
+  @UseGuards(AuthGuard(), PermissionsGuard)
+  @Permissions('dashboards.leer')
+  getDashboardCategories(@Req() req) {
+    return this.dashboardsService.getSalesByCategory(req.user);
+  }
 
-    @Get('sales')
-    @UseGuards(AuthGuard(), PermissionsGuard)
-    @Permissions('dashboards.leer')
-    getSalesDashboard(@Req() req, @Query('period') period: 'day' | 'week' | 'month' | 'year') {
-       return this.dashboardsService.getSalesDashboard(req.user, period);
-    }
+  @Get('sales')
+  @UseGuards(AuthGuard(), PermissionsGuard)
+  @Permissions('dashboards.leer')
+  getSalesDashboard(
+    @Req() req,
+    @Query('period') period: 'day' | 'week' | 'month' | 'year',
+  ) {
+    return this.dashboardsService.getSalesDashboard(req.user, period);
+  }
 
-    @Get('rentability')
-    @UseGuards(AuthGuard(), PermissionsGuard)
-    @Permissions('dashboards.leer')
-    getRentability(@Req() req) {
-       return this.dashboardsService.getRentabilityDashboard(req.user);
-    }
+  @Get('rentability')
+  @UseGuards(AuthGuard(), PermissionsGuard)
+  @Permissions('dashboards.leer')
+  getRentability(@Req() req) {
+    return this.dashboardsService.getRentabilityDashboard(req.user);
+  }
+
+  @Get('inventory')
+  @UseGuards(AuthGuard(), PermissionsGuard)
+  @Permissions('dashboards.leer')
+  getInventoryDashboard(@Req() req) {
+    return this.dashboardsService.getInventoryDashboard(req.user);
+  }
 }
--- src/dashboards/dashboards.service.ts
+++ src/dashboards/dashboards.service.ts
@@ -7,152 +7,190 @@ import { Shop } from 'src/schemas/Shop.schema';
 
 @Injectable()
 export class DashboardsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+    @InjectModel(Sale.name) private saleModel: Model<Sale>,
+  ) {}
 
-    constructor(
-        @InjectModel(Product.name) private productModel: Model<Product>,
-        @InjectModel(Sale.name) private saleModel: Model<Sale>,
-    ) {}
+  getSalesByCategory(shop: Shop) {
+    return this.productModel.aggregate([
+      {
+        $match: {
+          shop: shop._id,
+        },
+      },
+      {
+        $lookup: {
+          from: 'categories',
+          localField: 'category',
+          foreignField: '_id',
+          as: 'category',
+        },
+      },
+      {
+        $unwind: '$category',
+      },
+      {
+        $group: {
+          _id: '$category.name',
+          totalSales: { $sum: '$sales' },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalSalesAllCategories: { $sum: '$totalSales' },
+          categories: {
+            $push: {
+              category: '$_id',
+              totalSales: '$totalSales',
+            },
+          },
+        },
+      },
+      {
+        $unwind: '$categories',
+      },
+      {
+        $project: {
+          _id: '$categories.category',
+          sales: '$categories.totalSales',
+          percentage: {
+            $divide: ['$categories.totalSales', '$totalSalesAllCategories'],
+          },
+        },
+      },
+    ]);
+  }
 
-    getSalesByCategory(shop: Shop) {
-        return this.productModel.aggregate([
-            {
-              "$match": {
-                "shop": shop._id
-              }
-            },
-            {
-              "$lookup": {
-                "from": "categories",
-                "localField": "category",
-                "foreignField": "_id",
-                "as": "category"
-              }
-            },
-            {
-              "$unwind": "$category"
-            },
-            {
-              "$group": {
-                "_id": "$category.name",
-                "totalSales": { "$sum": "$sales" }
-              }
-            },
-            {
-              "$group": {
-                "_id": null,
-                "totalSalesAllCategories": { "$sum": "$totalSales" },
-                "categories": {
-                  "$push": {
-                    "category": "$_id",
-                    "totalSales": "$totalSales"
-                  }
-                }
-              }
-            },
-            {
-              "$unwind": "$categories"
-            },
-            {
-              "$project": {
-                "_id": "$categories.category",
-                "sales": "$categories.totalSales",
-                "percentage": {
-                  "$divide": ["$categories.totalSales", "$totalSalesAllCategories"]
-                }
-              }
-            }
-          ]);
-    }
+  getSalesDashboard(shop: Shop, period: 'day' | 'week' | 'month' | 'year') {
+    if (
+      period !== 'day' &&
+      period !== 'week' &&
+      period !== 'month' &&
+      period !== 'year'
+    )
+      throw new HttpException('Invalid period', 400);
 
-    getSalesDashboard(shop: Shop, period: "day" | "week" | "month" | "year") {
+    const dateFilter = {
+      day: {
+        filter: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+        format: '%H:00',
+      },
+      week: {
+        filter: new Date(new Date().setDate(new Date().getDate() - 7)),
+        format: '%m-%d',
+      },
+      month: {
+        filter: new Date(new Date().setDate(new Date().getDate() - 30)),
+        format: '%m-%d',
+      },
+      year: {
+        filter: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+        format: '%Y-%m',
+      },
+    };
 
-        if (period !== "day" && period !== "week" && period !== "month" && period !== "year") throw new HttpException('Invalid period', 400);
-
-        const dateFilter = {
-            "day": { filter: new Date(new Date().getTime() - 24*60*60*1000), format: "%H:00" },
-            "week": { filter: new Date(new Date().setDate(new Date().getDate() - 7)), format: "%m-%d" },
-            "month": { filter: new Date(new Date().setDate(new Date().getDate() - 30)), format: "%m-%d" },
-            "year": { filter: new Date(new Date().setFullYear(new Date().getFullYear() - 1)), format: "%Y-%m" }
-        };
-
-        return this.saleModel.aggregate([
-            {
-              "$match": {
-                "createdAt": {
-                  "$gte": dateFilter[period].filter
-                },
-                "status": "paid",
-                "shop": shop._id
-              }
+    return this.saleModel.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: dateFilter[period].filter,
+          },
+          status: 'paid',
+          shop: shop._id,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: dateFilter[period].format,
+              date: '$createdAt',
+              timezone: 'America/Lima',
             },
-            {
-              "$group": {
-                "_id": {
-                  "$dateToString": {
-                    "format": dateFilter[period].format,
-                    "date": "$createdAt",
-                    "timezone": "America/Lima"
-                  }
-                },
-                "sales": { "$sum": 1 }
-              }
-            },
-            {
-              "$sort": { "_id": 1 }
-            }
-          ]);
-    }
+          },
+          sales: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { _id: 1 },
+      },
+    ]);
+  }
 
-    async getRentabilityDashboard(shop: Shop) {
-        return this.productModel.aggregate(
-          [
+  async getRentabilityDashboard(shop: Shop) {
+    return this.productModel.aggregate([
+      {
+        $match: {
+          shop: shop._id,
+        },
+      },
+      {
+        $lookup: {
+          from: 'sales',
+          let: { productCode: '$code' },
+          pipeline: [
+            { $unwind: '$products' },
             {
               $match: {
-                shop: shop._id
-              }
+                $expr: {
+                  $and: [
+                    { $eq: ['$products.code', '$$productCode'] },
+                    { $eq: ['$status', 'paid'] },
+                    { $eq: ['$shop', shop._id] },
+                  ],
+                },
+              },
             },
             {
-              $lookup: {
-                from: "sales",
-                let: { productCode: "$code" },
-                pipeline: [
-                  { $unwind: "$products" },
-                  { $match: {
-                      $expr: {
-                        $and: [
-                          { $eq: ["$products.code", "$$productCode"] },
-                          { $eq: ["$status", "paid"] },
-                          { $eq: ["$shop", shop._id]}
-                        ]
-                      }
-                    }
-                  },
-                  { $group: {
-                      _id: null,
-                      rentability: { $sum: "$products.rentability" }
-                    }
-                  }
-                ],
-                as: "salesData"
-              }
+              $group: {
+                _id: null,
+                rentability: { $sum: '$products.rentability' },
+              },
             },
-            {
-              $addFields: {
-                rentability: {
-                  $ifNull: [{ $arrayElemAt: ["$salesData.rentability", 0] }, 0]
-                }
-              }
-            },
-            {
-              $project: {
-                _id: 0,
-                code: 1,
-                name: 1,
-                rentability: 1
-              }
-            }
-          ]
-        )
+          ],
+          as: 'salesData',
+        },
+      },
+      {
+        $addFields: {
+          rentability: {
+            $ifNull: [{ $arrayElemAt: ['$salesData.rentability', 0] }, 0],
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          code: 1,
+          name: 1,
+          rentability: 1,
+        },
+      },
+    ]);
+  }
 
-    } 
+  async getInventoryDashboard(shop: Shop) {
+    return this.productModel.aggregate([
+      {
+        $match: {
+          shop: shop._id,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          code: 1,
+          name: 1,
+          stock: 1,
+        },
+      },
+      {
+        $sort: { stock: -1 },
+      }
+    ]);
+
+  }
+
 }
