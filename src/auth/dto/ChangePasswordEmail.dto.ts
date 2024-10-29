edacbed793a2de8ef9379d24c365a84f82f34dbd
Gonzalo Sakuda <gonchissn@gmail.com>
2024-10-29 00:32:07 -0500

    Add validator

--- src/auth/dto/ChangePasswordEmail.dto.ts
+++ src/auth/dto/ChangePasswordEmail.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class ChangePasswordEmailDto {
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
   email: string;
 }
