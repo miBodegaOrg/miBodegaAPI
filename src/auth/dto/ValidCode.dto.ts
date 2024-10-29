a4be407c9c89247dc122025419abe4cf75c4bb10
Gonzalo Sakuda <gonchissn@gmail.com>
2024-10-29 00:30:52 -0500

    Add validations to dto

--- src/auth/dto/ValidCode.dto.ts
+++ src/auth/dto/ValidCode.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class ValidCodeDto {
+  @IsNotEmpty()
+  @IsString()
   code: string;
+
+  @IsNotEmpty()
+  @IsString()
   email: string;
 }
