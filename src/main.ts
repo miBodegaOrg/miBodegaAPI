7d3785f8a0ad9581547166008e8a1f5d9d332e2a
Gonzalo Sakuda <gonchissn@gmail.com>
2024-11-11 19:50:29 -0500

    Update port

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(80, '0.0.0.0');
 }
 bootstrap();
